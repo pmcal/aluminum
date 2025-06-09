13d0ec96281b3bac89fb8e6d3348d28656cdd127
Pedro Cal <p.m.rocio.cal@gmail.com>
2025-06-09 18:09:07 +0200

    Add f32 and f64 tests

--- aluminum/src/main.rs
+++ aluminum/src/main.rs
@@ -28,4 +28,20 @@ fn main() {
     // Scalar multiplication
     let scalar_prod = 2.5 * &a;
     println!("a * 2.5 =\n{}", scalar_prod);
+
+    // f32 test
+    let a32 = Mat::new(2, 2, vec![1.0f32, 3.0, 2.0, 4.0]);
+    let b32 = Mat::from_vec(2, 2, vec![5.0f32, 7.0, 6.0, 8.0]);
+    let ab32_sum = &a32 + &b32;
+    println!("a32 + b32 =\n{:?}", ab32_sum);
+    let ab32_prod = &a32 * &b32;
+    println!("a32 * b32 =\n{:?}", ab32_prod);
+
+    // f64 test
+    let a64 = Mat::new(2, 2, vec![1.0f64, 3.0, 2.0, 4.0]);
+    let b64 = Mat::from_vec(2, 2, vec![5.0f64, 7.0, 6.0, 8.0]);
+    let ab64_sum = &a64 + &b64;
+    println!("a64 + b64 =\n{:?}", ab64_sum);
+    let ab64_prod = &a64 * &b64;
+    println!("a64 * b64 =\n{:?}", ab64_prod);
 }
